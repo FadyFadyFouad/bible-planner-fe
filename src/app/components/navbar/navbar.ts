6ee53877beec6f655b5bd5371488c94ea61ba13b
Fady Fahim <fadyfadyfouad@gmail.com>
2026-02-21 12:18:32 +0200

    feat: add navbar component and integrate Bootstrap styles
    - Introduced a new Navbar component with HTML and SCSS files.
    - Updated app component to include the Navbar.
    - Added Bootstrap SCSS import to global styles.
    - Created Homepage and Planner components with basic templates.
    - Added tests for Navbar, Homepage, and Planner components.
    - Configured TypeScript to include Angular localization types.

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgbNavModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+
+}
